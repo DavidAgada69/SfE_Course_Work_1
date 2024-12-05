c3bd4fd302f60937b05a0831559a4a75edc7a985
Jerry Olaojo <Jerryojo178@gmail.com>
2024-12-05 05:41:11 +0100

    I made some changes to the html and css files to make it look cleaner and fixed all image issues.

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const wrapper = document.querySelector(".sliderWrapper")
 
 console.log(wrapper)
 
+// Get all menu items and the sliderWrapper
+const menuItems = document.querySelectorAll('.menuItem');
+const sliderWrapper = document.querySelector('.sliderWrapper');
+
+// Add click events to navigation items
+menuItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        // Calculate the translateX value based on the index
+        const offset = index * window.innerWidth;
+        sliderWrapper.style.transform = `translateX(-${offset}px)`;
+    });
+});
